ee099256d4890b664898b3b6ca21da0fe92cec73
권장순 <jsoonworld@gmail.com>
2025-04-14 15:56:33 +0900

    feat(OutputView) : 상품 재고 출력 기능 구현

--- src/main/kotlin/store/view/OutputView.kt
+++ src/main/kotlin/store/view/OutputView.kt
@@ -1,5 +1,8 @@
 package store.view
 
+import store.domain.Product
+import store.parser.ProductFormatter
+
 class OutputView {
     private val WELCOME_MESSAGE: String = "안녕하세요. W편의점입니다."
     private val STOCK_CONDITION_MESSAGE: String = "현재 보유하고 있는 상품입니다."
@@ -10,5 +13,11 @@ class OutputView {
 
     fun printStockConditionMessage() {
         println(STOCK_CONDITION_MESSAGE)
+    }
+
+    fun printProducts(products: List<Product>, formatter: ProductFormatter) {
+        products.forEach { product ->
+            println(formatter.format(product))
+        }
     }
 }
