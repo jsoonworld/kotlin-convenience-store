bc26d4cabd143a916f58c7286c9364030000f441
권장순 <jsoonworld@gmail.com>
2025-04-14 14:29:41 +0900

    feat(OutputView) : 재고 현황을 보여주는 안내 메시지 기능 구현

--- src/main/kotlin/store/view/OutputView.kt
+++ src/main/kotlin/store/view/OutputView.kt
@@ -2,8 +2,13 @@ package store.view
 
 class OutputView {
     private val WELCOME_MESSAGE: String = "안녕하세요. W편의점입니다."
+    private val STOCK_CONDITION_MESSAGE: String = "현재 보유하고 있는 상품입니다."
 
     fun printWelcomeMessage() {
-        print(WELCOME_MESSAGE)
+        println(WELCOME_MESSAGE)
+    }
+
+    fun printStockConditionMessage() {
+        println(STOCK_CONDITION_MESSAGE)
     }
 }
