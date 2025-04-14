88d3a9248c4f828603e0e1dd19940e44cb23d287
권장순 <jsoonworld@gmail.com>
2025-04-14 22:29:44 +0900

    feat(SimpleProductFormatter) : 재고 0일 경우 '재고 없음'으로 출력 형식 변경

--- src/main/kotlin/store/parser/SimpleProductFormatter.kt
+++ src/main/kotlin/store/parser/SimpleProductFormatter.kt
@@ -2,12 +2,12 @@ package store.parser
 
 import store.domain.Product
 import java.text.NumberFormat
-import java.util.Locale
+import java.util.*
 
 class SimpleProductFormatter : ProductFormatter {
     override fun format(product: Product): String {
         val price = NumberFormat.getNumberInstance(Locale.KOREA).format(product.price)
-        val quantity = "${product.quantity}개"
+        val quantity = if (product.quantity == 0) "재고 없음" else "${product.quantity}개"
         val base = "- ${product.name} ${price}원 $quantity"
         return if (product.promotionName.isPresent()) "$base ${product.promotionName}" else base
     }
