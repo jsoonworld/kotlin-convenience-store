a0688a994263406a1d17940991f2e11b4c286240
권장순 <jsoonworld@gmail.com>
2025-04-14 16:39:08 +0900

    feat(OutputView) : 멤버십 할인 안내 문구 기능 구현

--- src/main/kotlin/store/view/OutputView.kt
+++ src/main/kotlin/store/view/OutputView.kt
@@ -7,6 +7,7 @@ class OutputView {
     private val WELCOME_MESSAGE: String = "안녕하세요. W편의점입니다."
     private val STOCK_CONDITION_MESSAGE: String = "현재 보유하고 있는 상품입니다."
     private val PURCHASE_INTRODUCTION_MESSAGE: String = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"
+    private val MEMBERSHIP_DISCOUNT_INTRODUCTION_MESSAGE: String = "멤버십 할인을 받으시겠습니까? (Y/N)"
 
     fun printWelcomeMessage() {
         println(WELCOME_MESSAGE)
@@ -24,5 +25,9 @@ class OutputView {
 
     fun printPurchaseIntroductionMessage() {
         println(PURCHASE_INTRODUCTION_MESSAGE)
+    }
+
+    fun printMembershipDiscountIntroductionMessage() {
+        println(MEMBERSHIP_DISCOUNT_INTRODUCTION_MESSAGE)
     }
 }
